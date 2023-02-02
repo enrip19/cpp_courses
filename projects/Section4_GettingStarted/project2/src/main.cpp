a2dd89dcf13e43ba7dd54562b4aa4d53862976d4
Enric Puigvert <enrip19@gmail.com>
2023-02-02 16:46:33 +0100

    Initial commit. All vscode configurations for C++ working. Clang-format working. Section 4 (Getting started) finished with the challenges working correctly

---
+++ projects/Section4_GettingStarted/project2/src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int fav_number {100};
+    cout << "Hello World form project2" << endl;
+    cout << "Enter your favorite number: ";
+    cin >> fav_number;
+    cout << "Your favorite number is: " << fav_number << endl;
+    return 0;
+}
