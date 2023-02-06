a14a23ef465df72275129ff192fc8f9c68e8df1a
Enric Puigvert <enrip19@gmail.com>
2023-02-06 09:46:20 +0100

    ZBOOK  vault backup: 2023-02-06 09:46:20

---
+++ projects/Section5_StructureOfC++Program/BasicIO/src/main.cpp
@@ -0,0 +1,27 @@
+// Section 5
+// Basic I/O using cin and cout
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  // example 1
+//   cout << "Hello world!";
+
+  // example2
+    cout << "Hello";
+    cout << "World" << endl;
+
+  // example 3
+  //   cout << "Hello World!" << end;
+  //   cout << "Hello"
+  //        << "world!" << endl;
+  //   cout << "Hello"
+  //        << "world!\n";
+  //   cout << "Hello\nOut\nThere\n";
+
+  // example 4
+  //   int num1;
+  //   int num2;
+}
