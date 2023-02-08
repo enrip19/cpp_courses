2b984f1ae93740797a0d2e9dd1cae3e6f018794b
Enric Puigvert <enrip19@gmail.com>
2023-02-08 10:02:16 +0100

    ZBOOK  vault backup: 2023-02-08 10:02:16

---
+++ examples/Section-4-Getting-Started-Source-code/Section4/Challenge/src/main.cpp
@@ -0,0 +1,42 @@
+/******************************************************************************
+Section 4 Challenge
+=============
+
+Create a C++ program that asks the user for their favorite number between 1 and
+100 then read this number from the console.
+
+Suppose the user enters 24.
+
+Then display the following to the console:
+
+Amazing!! That's my favorite number too!
+No really!!, 24 is my favorite number!
+
+
+Below are 2 sample runs of the program:
+=======================================
+Enter your favorite number between 1 and 100: 24
+Amazing!! That's my favorite number too!
+No really!!, 24 is my favorite number!
+
+
+Enter your favorite number between 1 and 100: 75
+Amazing!! That's my favorite number too!
+No really!!, 75 is my favorite number!%
+
+******************************************************************************/
+
+#include <iostream>
+
+int main() {
+  int fav_num;
+  std::cout << "Choose your favourite number between 1 and 100: ";
+
+  std::cin >> fav_num;
+
+  std::cout << "Amazing!! That's my favorite number too!" << std::endl;
+  std::cout << "No really!!, " << fav_num << " is my favorite number!"
+            << std::endl;
+
+  return 0;
+}
