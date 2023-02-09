f91c9d244abc3b3dca5d3fa9195479810a778891
Enric Puigvert <enrip19@gmail.com>
2023-02-09 15:01:50 +0100

    ZBOOK  vault backup: 2023-02-09 15:01:50

--- examples/Section-8-Statements-and-Operators-Source-code/Section8/Challenge/main.cpp
+++ examples/Section-8-Statements-and-Operators-Source-code/Section8/Challenge/main.cpp
@@ -1,36 +1,38 @@
 // Section 8 Challenge
 /*
-	For this program I will be using US dollars and cents.
-	
-    Write a program that asks the user to enter the following:
-	An integer representing the number of cents 
-    	
-	You may assume that the number of cents entered is greater than or equal to zero
-    
-	The program should then display how to provide that change to the user.
-	
-	In the US:
-		1 dollar is 100 cents
-		1 quarter is 25 cents
-		1 dime is 10 cents
-		1 nickel is 5 cents, and
-		1 penny is 1 cent.
-		
-	Here is a sample run:
-	
-	Enter an amount in cents :  92
-	
-	You can provide this change as follows:
-	dollars    : 0
-	quarters : 3
-	dimes     : 1
-	nickels   : 1
-	pennies  : 2
-	
-	Feel free to use your own currency system.
-	Also, think of how you might solve the problem using the modulo operator.
+        For this program I will be using US dollars and cents.
 
-	Have fun and test your program!!
+    Write a program that asks the user to enter the following:
+        An integer representing the number of cents
+
+        You may assume that the number of cents entered is greater than or equal
+   to zero
+
+        The program should then display how to provide that change to the user.
+
+        In the US:
+                1 dollar is 100 cents
+                1 quarter is 25 cents
+                1 dime is 10 cents
+                1 nickel is 5 cents, and
+                1 penny is 1 cent.
+
+        Here is a sample run:
+
+        Enter an amount in cents :  92
+
+        You can provide this change as follows:
+        dollars    : 0
+        quarters : 3
+        dimes     : 1
+        nickels   : 1
+        pennies  : 2
+
+        Feel free to use your own currency system.
+        Also, think of how you might solve the problem using the modulo
+   operator.
+
+        Have fun and test your program!!
 
 */
 #include <iostream>
@@ -38,9 +40,16 @@
 using namespace std;
 
 int main() {
+  int cents, dollars, quarters, dimes, nickels, pennies;
+	const int cents_to_dollars = 100;
+	const int 
+  cout << "Enter an amount in cents: ";
+  cin >> cents;
 
-    cout << endl;
-    return 0;
+  dollars = cents / 100;
+  cents %= 100;
+  cout << dollars << cents;
+
+  cout << endl;
+  return 0;
 }
-
-
