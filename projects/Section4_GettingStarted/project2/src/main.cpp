911f9503b2aaadba5a4eedd2206385c0619da5f4
Enric Puigvert <enrip19@gmail.com>
2023-02-09 15:31:50 +0100

    ZBOOK  vault backup: 2023-02-09 15:31:50

--- projects/Section4_GettingStarted/project2/src/main.cpp
+++ projects/Section4_GettingStarted/project2/src/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 using namespace std;
 int main() {
-  int fav_number{100};
-  cout << "Hello World form project2" << endl;
-  cout << "Enter your favorite number: ";
-  cin >> fav_number;
-  cout << "Your favorite number is: " << fav_number << endl;
+  // int fav_number{100};
+  // cout << "Hello World form project2" << endl;
+  // cout << "Enter your favorite number: ";
+  // cin >> fav_number;
+  // cout << "Your favorite number is: " << fav_number << endl;
+  cout << type("Hola");
   return 0;
 }
