b007b041164af58726af00ed8e0197dec0821206
Enric Puigvert <enrip19@gmail.com>
2023-02-10 14:49:35 +0100

    ZBOOK  vault backup: 2023-02-10 14:49:35

--- examples/Section-10-Characters-and-Strings-Source-code/Section10/Challenge/main.cpp
+++ examples/Section-10-Characters-and-Strings-Source-code/Section10/Challenge/main.cpp
@@ -1,29 +1,31 @@
 // Section 10
-// Challenge - 
+// Challenge -
 // Substitution Cipher
 
 /*
-A simple and very old method of sending secret messages is the substitution cipher.
-You might have used this cipher with your friends when you were a kid.
-Basically, each letter of the alphabet gets replaced by another letter of the alphabet.
-For example, every 'a' get replaced with an 'X', and every 'b' gets replaced with a 'Z', etc.
+A simple and very old method of sending secret messages is the substitution
+cipher. You might have used this cipher with your friends when you were a kid.
+Basically, each letter of the alphabet gets replaced by another letter of the
+alphabet. For example, every 'a' get replaced with an 'X', and every 'b' gets
+replaced with a 'Z', etc.
 
 Write a program thats ask a user to enter a secret message.
 
-Encrypt this message using the substitution cipher and display the encrypted message.
-Then decryped the encrypted message back to the original message.
+Encrypt this message using the substitution cipher and display the encrypted
+message. Then decryped the encrypted message back to the original message.
 
 You may use the 2 strings below for  your subsitition.
-For example, to encrypt you can replace the character at position n in alphabet 
+For example, to encrypt you can replace the character at position n in alphabet
 with the character at position n in key.
 
 To decrypt you can replace the character at position n in key
 with the character at position n in alphabet.
 
 Have fun! And make the cipher stronger if you wish!
-Currently the cipher only substitutes letters - you could easily add digits, puncuation, whitespace and so
-forth. Also, currently the cipher always substitues a lowercase letter with an uppercase letter and vice-versa.
-This could also be improved.
+Currently the cipher only substitutes letters - you could easily add digits,
+puncuation, whitespace and so forth. Also, currently the cipher always
+substitues a lowercase letter with an uppercase letter and vice-versa. This
+could also be improved.
 
 Remember, the less code you write the less code you have to test!
 Reuse existing functionality in libraries and in the std::string class!
@@ -35,23 +37,34 @@ Reuse existing functionality in libraries and in the std::string class!
 using namespace std;
 
 int main() {
-    
-    string alphabet {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
-    string key  {"XZNLWEBGJHQDYVTKFUOMPCIASRxznlwebgjhqdyvtkfuompciasr"};
+  string alphabet{"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+  string key{"XZNLWEBGJHQDYVTKFUOMPCIASRxznlwebgjhqdyvtkfuompciasr"};
 
-    string secret_message;
-    string encrypted_message;
+  string secret_message, encrypted_message, decrypted_message;
 
-    cout << "Write a secret message: ";
-    cin >> secret_message;
+  cout << "Write a secret message: ";
+  getline(cin, secret_message);
+  cout << endl;
 
-    for (auto c: secret_message)
-    {
-        encrypted_message += key[alphabet.find(c)
-    }
+  for (auto c : secret_message) {
+    char k = key[alphabet.find(c)];
+    if (k != string::npos)
+      encrypted_message += k;
+    else
+      encrypted_message += c;
+  }
+  cout << "Encripted message: " << encrypted_message << endl;
 
-    
-    cout << endl;
-    return 0;
+  for (auto c : encrypted_message) {
+    char a = alphabet[key.find(c)];
+    if (a != string::npos)
+      decrypted_message += a;
+    else
+      decrypted_message += c;
+  }
+
+  cout << "Decrypted message: " << decrypted_message << endl;
+
+  cout << endl;
+  return 0;
 }
-
