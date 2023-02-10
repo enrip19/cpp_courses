a8a26ccc8e046982366a6a8f208106a5a216fdc2
Enric Puigvert <enrip19@gmail.com>
2023-02-10 15:59:35 +0100

    ZBOOK  vault backup: 2023-02-10 15:59:35

--- examples/Section-10-Characters-and-Strings-Source-code/Section10/Assignment/src/main.cpp
+++ examples/Section-10-Characters-and-Strings-Source-code/Section10/Assignment/src/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main() {
   string input_string;
 
-  cout << "Enter a string for Letter Pyramid construction: " << endl;
+  //   cout << "Enter a string for Letter Pyramid construction: ";
   //   cin >> input_string;
   input_string = {"abc"};
   cout << "### Debug ###\t Input String: " << input_string << endl;
@@ -17,31 +17,30 @@ int main() {
                                          // lines will have the pyramid
   int base_len = string_len * 2 - 1;
 
-  string reverse_string = string(input_string.rbegin(), input_string.rend());
-  string base = input_string + reverse_string.substr(1, reverse_string.size());
-
-  cout << "### Debug ###\tBase length: " << base_len << " vs " << base.size()
-       << endl;
-
-  cout << "### Debug ###\tBASE:" << base << endl;
-
   for (int i = 1; i <= string_len; i++) {
-    string line_chars = base.substr(0, i);
-    string pyramid_line = line_chars;
+    string line_chars = input_string.substr(0, i);
+    string reverse_line_chars = string(line_chars.rbegin(), line_chars.rend());
 
-    int num_spaces = string_len - line_chars.size();
+    string line =
+        line_chars + reverse_line_chars.substr(1, reverse_line_chars.size());
+    // cout << "### Debug ###\tline: " << line << endl;
 
-    cout << "### Debug ###\tline_chars: " << line_chars
-         << "\tnum_spaces: " << num_spaces << endl;
+    string pyramid_line = line;
+
+    int num_spaces = base_len - line.size();
+
+    // cout << "### Debug ###\tline_chars: " << line_chars
+    //      << "\tnum_spaces: " << num_spaces << endl;
 
     if (num_spaces > 0) {
       int spaces_every_sid = num_spaces / 2;
 
-      for (int j = 0; j < spaces_every_sid; j++) {
+      for (int j = 0; j <= spaces_every_sid; j++) {
         pyramid_line.insert(j, {" "});
-        pyramid_line.push_back(' ');
       }
+
       cout << pyramid_line << endl;
+
     } else
       cout << pyramid_line << endl;
   }
