a323b49047409fc3668721448d46b0330ee50c49
Enric Puigvert <enrip19@gmail.com>
2023-02-10 15:49:35 +0100

    ZBOOK  vault backup: 2023-02-10 15:49:35

--- examples/Section-10-Characters-and-Strings-Source-code/Section10/Assignment/src/main.cpp
+++ examples/Section-10-Characters-and-Strings-Source-code/Section10/Assignment/src/main.cpp
@@ -6,10 +6,10 @@ using namespace std;
 int main() {
   string input_string;
 
-  cout << "Enter a string for Letter Pyramid construction: ";
+  cout << "Enter a string for Letter Pyramid construction: " << endl;
   //   cin >> input_string;
   input_string = {"abc"};
-  cout << endl;
+  cout << "### Debug ###\t Input String: " << input_string << endl;
 
   cout << "\n --- Letter Pyramid --- " << endl;
 
@@ -25,25 +25,25 @@ int main() {
 
   cout << "### Debug ###\tBASE:" << base << endl;
 
-  for (int i = 0; i <= string_len; i++) {
-    string line_chars = input_string.substr(0, i);
-    int num_spaces = string_len - line_chars.size() - 1;
-    
-    cout << line_chars << ", " << num_spaces << endl;
+  for (int i = 1; i <= string_len; i++) {
+    string line_chars = base.substr(0, i);
+    string pyramid_line = line_chars;
 
+    int num_spaces = string_len - line_chars.size();
+
+    cout << "### Debug ###\tline_chars: " << line_chars
+         << "\tnum_spaces: " << num_spaces << endl;
 
     if (num_spaces > 0) {
       int spaces_every_sid = num_spaces / 2;
-
-      string pyramid_line = line_chars;
 
       for (int j = 0; j < spaces_every_sid; j++) {
         pyramid_line.insert(j, {" "});
         pyramid_line.push_back(' ');
       }
-      cout << "pyramid_line: " << pyramid_line << endl;
-    }
-
+      cout << pyramid_line << endl;
+    } else
+      cout << pyramid_line << endl;
   }
 
   cout << endl;
