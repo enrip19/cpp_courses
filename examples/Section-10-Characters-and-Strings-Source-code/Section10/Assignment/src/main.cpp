d7e563ec5736130e2769405eb42dc081a783f4cf
Enric Puigvert <enrip19@gmail.com>
2023-02-10 15:19:35 +0100

    ZBOOK  vault backup: 2023-02-10 15:19:35

--- examples/Section-10-Characters-and-Strings-Source-code/Section10/Assignment/src/main.cpp
+++ examples/Section-10-Characters-and-Strings-Source-code/Section10/Assignment/src/main.cpp
@@ -4,22 +4,27 @@
 using namespace std;
 
 int main() {
+  string input_string;
 
-    string input_string;
+  cout << "Enter a string for Letter Pyramid construction: ";
+  cin >> input_string;
+  cout << endl;
 
-    cout << "Enter a string for Letter Pyramid construction: ";
-    cin >> input_string;
-    cout << endl;
+  cout << "\n --- Letter Pyramid --- " << endl;
 
-    cout << "\n --- Letter Pyramid --- " << endl;
+  int string_len = input_string.size();
+  int base_len = string_len * 2 - 1;
 
-    int string_len = input_string.size();
+  string reverse_string = string(input_string.rbegin(), input_string.rend());
+  string base = input_string + reverse_string.substr(1, reverse_string.size());
 
+  cout << "### Debug ###\tBase length: " << base_len << " vs " << base.size()
+       << endl;
 
-    for (int i=0; i<((string_len*2)-1); i++)
-    {
-        cout << 
-    }
+  cout << "### Debug ###\tBASE:" << base << endl;
 
+  for (int i = 0; i < ((string_len * 2) - 1); i++) {
+  }
 
+  cout << endl;
 }
