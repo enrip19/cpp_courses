5b9719eddcf101fdb2a4d377881fe5a4b707be7d
Enric Puigvert <enrip19@gmail.com>
2023-02-10 15:09:35 +0100

    ZBOOK  vault backup: 2023-02-10 15:09:35

---
+++ examples/Section-10-Characters-and-Strings-Source-code/Section10/Assignment/src/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    string input_string;
+
+    cout << "Enter a string for Letter Pyramid construction: ";
+    cin >> input_string;
+    cout << endl;
+
+    cout << "\n --- Letter Pyramid --- " << endl;
+
+    int string_len = input_string.size();
+
+
+    for (int i=0; i<((string_len*2)-1); i++)
+    {
+        cout << 
+    }
+
+
+}
